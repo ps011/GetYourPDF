c54c5725daf4535484b51ea244848c0f56f30bea
Prasheel Soni <prasheelsoni11@gmail.com>
2016-06-18 21:45:16 +0530

    fix : mailer

--- mailer.php
+++ mailer.php
@@ -1,35 +1,37 @@
 <?php
 
- require_once('class.phpmailer.php');
+ // require_once('class.phpmailer.php');
+ //
+ //    $mail = new PHPMailer();
+ //    $mail->CharSet =  "utf-8";
+ //    $mail->IsSMTP();
+ //    $mail->SMTPAuth = true;
+ //    $mail->Username = "prasheelsoni11@gmail.com";
+ //    $mail->Password = "37Goldy37!";
+ //    $mail->SMTPSecure = "ssl";
+ //    $mail->Host = "smtp.gmail.com";
+ //    $mail->Port = "465";
+ //
+ //    $mail->setFrom('prasheelsoni11@gmail.com', ' Prasheel');
+ //    $mail->AddAddress('prasheelsoni11@mail.com', ' Goldy');
+ //
+ //    $mail->Subject  =  'using PHPMailer';
+ //    $mail->IsHTML(true);
+ //    $mail->Body    = 'Hi there ,
+ //                        <br />
+ //                        this mail was sent using PHPMailer...
+ //                        <br />
+ //                        cheers... :)';
+ //
+ //     if($mail->Send())
+ //     {
+ //        echo "Message was Successfully Send :)";
+ //     }
+ //     else
+ //     {
+ //        echo "Mail Error - >".$mail->ErrorInfo;
+ //     }
+mail('prasheelsoni11@gmail.com','Test','Body');
 
-    $mail = new PHPMailer();
-    $mail->CharSet =  "utf-8";
-    $mail->IsSMTP();
-    $mail->SMTPAuth = true;
-    $mail->Username = "prasheelsoni11@gmail.com";
-    $mail->Password = "37Goldy37!";
-    $mail->SMTPSecure = "ssl";
-    $mail->Host = "smtp.gmail.com";
-    $mail->Port = "465";
-
-    $mail->setFrom('prasheelsoni11@gmail.com', ' Prasheel');
-    $mail->AddAddress('prasheelsoni11@mail.com', ' Goldy');
-
-    $mail->Subject  =  'using PHPMailer';
-    $mail->IsHTML(true);
-    $mail->Body    = 'Hi there ,
-                        <br />
-                        this mail was sent using PHPMailer...
-                        <br />
-                        cheers... :)';
-
-     if($mail->Send())
-     {
-        echo "Message was Successfully Send :)";
-     }
-     else
-     {
-        echo "Mail Error - >".$mail->ErrorInfo;
-     }
 
 ?>
