3ec0cf34ff29ea3c9231a419cec857cb323584a3
Puneet Matta <punit.matta1995@gmail.com>
2016-06-20 16:39:14 +0530

    added:books

--- download.php
+++ download.php
@@ -4,29 +4,45 @@ $download = $_GET['d'];
 $_SESSION['download'] = $download;
 
 
-if($download == 501)
+if($download == 101)
 {
-header('location:501.pdf');
+header('location:pdf/Engineering Chemistry.pdf');
 }
-if($download == 502)
+if($download == 102)
 {
-header('location:502.pdf');
+header('location:pdf/Engineering Maths.pdf');
 }
-if($download == 503)
+if($download == 103)
 {
-header('location:503.pdf');
+header('location:pdf/Communication Skills.pdf');
 }
-if($download == 504)
+if($download == 104)
+{
+header('location:pdf/BEEE.pdf');
+}
+if($download == 105)
+{
+header('location:pdf/Engineering Drawing.pdf');
+}
+if($download == 201)
+{
+header('location:pdf/Engineering Physics');
+}
+if($download == 202)
+{
+header('location:pdf/Environmental_book');
+}
+if($download == 203)
+{
+header('location:pdf/Mech Engg.pdf');
+}
+if($download == 204)
+{
+header('location:pdf/Civil Engg.pdf');
+}
+if($download == 205)
 {
 header('location:504.pdf');
-}
-if($download == 505)
-{
-header('location:505.pdf');
-}
-if($download == 506)
-{
-header('location:506.pdf');
 }
 
 ?>
